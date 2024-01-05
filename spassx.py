602bcd4c99f6c7fb6d6bc7a87601ac95963507a1
Singhumd <154690660+Singhumd@users.noreply.github.com>
2024-01-05 10:01:08 +0530

    spassx.py

--- spassx.py
+++ spassx.py
@@ -1,4 +1,4 @@
-#           Random Password Generator using Python
+#Random Password Generator using Python
 
 import random
 
@@ -9,17 +9,17 @@ digit="0123456"
 upper,lower,sym,dig = True,True,True,True
 password=""
 if upper:
-    password=password + upper_letters + lower_letters + symbols + digit
-elif lower:
-    password=password + lower_letters + symbols + digits
-elif sym:
-    password= password + symbol + digit
-else :
+    password=password + upper_letters 
+if lower:
+    password=password + lower_letters 
+if sym:
+    password= password + symbols
+if dig :
     password=password + digit
 
 strength = 13
 amount = 11
-print("\n Our Required Possible passwords\t")
+print("\n Our Required Possible Generated passwords From Gievn Inputs Are : \t")
 
 for x in range(amount):
   
